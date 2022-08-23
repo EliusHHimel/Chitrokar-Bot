fab54e9b31092381295d23ce55e44d0d1d004eea
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2022-08-24 00:46:09 +0600

    removed unnecessary logs

--- features/WebCapture.py
+++ features/WebCapture.py
@@ -3,10 +3,6 @@ from decouple import config
 
 WEB_SS_API_KEY = config('WEB_SS_API_KEY')
 
-url = "https://shot.screenshotapi.net/screenshot?token="+WEB_SS_API_KEY + \
-    "&url=https%3A%2F%2Fgoogle.com&full_page=true&output=json&file_type=png&wait_for_event=load"
-print(url)
-
 
 async def webCap(message):
     if message.content.startswith('$wcap'):
