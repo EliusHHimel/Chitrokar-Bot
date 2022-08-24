5f993a28258e8bb09c214d09594a5b1bd3fca875
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2022-08-25 01:56:45 +0600

    unnecessary log delete

--- features/meme.py
+++ features/meme.py
@@ -7,6 +7,4 @@ async def getMeme(message):
     if message.content.startswith('$meme'):
         response = requests.get(
             'https://meme-api.herokuapp.com/gimme').json()
-        print(response)
         await message.reply(response['url'])
-
